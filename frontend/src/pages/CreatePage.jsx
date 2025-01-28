1e7d47bfa54cd02c8cc734db4acff55cdbe628d5
rofiqabdul <rofiqabdul04@gmail.com>
2025-01-28 09:31:57 +0700

    2. Frontend - Ceate Product

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,75 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
+
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createNewProduct } = useProductStore();
+  const handleAddProduct = async () => {
+    const { success, message } = await createNewProduct(newProduct);
+    console.log("Success: ", success);
+    console.log("Message: ", message);
+  };
+
   return (
-    <>
-      <h1>Create Page</h1>
-    </>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Create New Product
+        </Heading>
+        <Box
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          p={6}
+          rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              name="name"
+              value={newProduct.name}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, name: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Price"
+              name="price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, price: e.target.value })
+              }
+            />
+            <Input
+              placeholder="Image URL"
+              name="image"
+              value={newProduct.image}
+              onChange={(e) =>
+                setNewProduct({ ...newProduct, image: e.target.value })
+              }
+            />
+            <Button colorScheme={"blue"} onClick={handleAddProduct}>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   );
 };
 
---
+++ frontend/src/store/product.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+  products: [],
+  setProduct: (products) => set({ products }),
+  createNewProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.price || !newProduct.image) {
+      return { success: false, message: "Please provide all fields" };
+    }
+    const res = await fetch("api/product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+    const data = await res.json();
+    set((state) => ({ products: [...state.products, data.data] }));
+    return { success: true, message: "Product created successfully" };
+  },
+}));
